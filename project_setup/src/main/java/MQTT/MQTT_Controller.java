7751ce5fbe25b1402c12e7c3b6abf91787497ee5
Masonrocca21 <christian310021@gmail.com>
2025-05-31 01:02:22 +0200

    Added MQTT server

--- project_setup/src/main/java/MQTT/MQTT_Controller.java
+++ project_setup/src/main/java/MQTT/MQTT_Controller.java
@@ -12,10 +12,59 @@ public class MQTT_Controller {
     private static String subTopic;
     private static String pubTopic;
 
+    private static int subQos = 2;
+    private static int pubQos = 2;
+
+
     public static void main(String[] args) {
         broker = "tcp://localhost:1883";
         clientId = MqttClient.generateClientId();
-        subTopic = "home/sensors/temp";
+        subTopic = "home/renewableEnergyProvider/power";
         pubTopic = "home/controllers/temp";
+
+        try {
+            client = new MqttClient(broker, clientId);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+
+            System.out.println(clientId + " Connecting Broker " + broker);
+            client.connect(connOpts);
+            System.out.println(clientId + " Connected " + Thread.currentThread().getId());
+
+            client.setCallback(new MqttCallback() {
+
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    String time = new Timestamp(System.currentTimeMillis()).toString();
+                    String receivedMessage = new String(message.getPayload());
+                    System.out.println(clientId + " Received a Message! - Callback - Thread PID: " + Thread.currentThread().getId() +
+                            "\n\tTime:    " + time +
+                            "\n\tTopic:   " + topic +
+                            "\n\tMessage: " + receivedMessage);
+                }
+
+                public void connectionLost(Throwable cause) {
+                    System.out.println(clientId + " Connectionlost! cause:" + cause.getMessage() + "-  Thread PID: " + Thread.currentThread().getId());
+                }
+
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    //Not used here
+                }
+            });
+
+            System.out.println(clientId + " Subscribing ... - Thread PID: " + Thread.currentThread().getId());
+            client.subscribe(subTopic, subQos);
+            System.out.println(clientId + " Subscribed to topics : " + subTopic);
+
+
+        } catch (MqttException me) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+
+
     }
 }
--- project_setup/src/main/java/RenewableEnergyProvider/RenewableEnergyProvider.java
+++ project_setup/src/main/java/RenewableEnergyProvider/RenewableEnergyProvider.java
@@ -1,13 +1,48 @@
 package RenewableEnergyProvider;
 
+import org.eclipse.paho.client.mqttv3.*;
+
 public class RenewableEnergyProvider {
 
-    private int time = 0;
     public RenewableEnergyProvider() {}
 
-    public int[] request (){
-        time++; //Cpire come gestire l tempo che passa
+    public static void main(String[] argv){
 
-        return new int[]{time - 1, (int)(Math.random()*(15000 - 5000 + 1) + 5000)};
+        String broker = "tcp://localhost:1883";
+        String clientId = MqttClient.generateClientId();
+        String topic = "home/renewableEnergyProvider/power";
+        int qos = 2;
+
+        try {
+            MqttClient client = new MqttClient(broker, clientId);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+
+            System.out.println(clientId + " Connecting Broker " + broker);
+            client.connect(connOpts);
+            System.out.println(clientId + " Connected");
+
+            //Every five seconds, generate the random value of temperature
+            while(true) {
+                String payload = String.valueOf((Math.random()*(15000 - 5000 + 1) + 5000));
+                MqttMessage message = new MqttMessage(payload.getBytes());
+                message.setQos(qos);
+                System.out.println(clientId + " Publishing message: " + payload + " ...");
+                client.publish(topic, message);
+                System.out.println(clientId + " Message published");
+                Thread.sleep(10000);
+            }
+
+        } catch (MqttException me ) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
